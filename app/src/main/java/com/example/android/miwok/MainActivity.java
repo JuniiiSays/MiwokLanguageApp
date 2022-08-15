33c5590b9ad1cc9ad206822b3180ce67affca853
Junaid Iqbal <Junaid.jigar761@gmail.com>
2022-08-15 20:16:25 +0500

    Intent added to open new activities

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,7 +15,9 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 // import android.support.v7.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,5 +29,25 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersList(View view) {
+        Intent numberIntent = new Intent(this, NumbersActivity.class);
+        startActivity(numberIntent);
+    }
+
+    public void openFamilyList(View view) {
+        Intent familyIntent = new Intent(this, FamilyActivity.class);
+        startActivity(familyIntent);
+    }
+
+    public void openColorList(View view) {
+        Intent colorIntent = new Intent(this, ColorsActivity.class);
+        startActivity(colorIntent);
+    }
+
+    public void openPhrasesList(View view) {
+        Intent phrasesIntent = new Intent(this, PhrasesActivity.class);
+        startActivity(phrasesIntent);
     }
 }
