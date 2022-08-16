4a0a91ce5eae6b32fd72426dc260d592de8618a4
Junaid Iqbal <Junaid.jigar761@gmail.com>
2022-08-16 18:37:33 +0500

    Click listener is implemented on Number view

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -18,8 +18,11 @@ package com.example.android.miwok;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 // import android.support.v7.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,25 +32,22 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
-    }
 
-    public void openNumbersList(View view) {
-        Intent numberIntent = new Intent(this, NumbersActivity.class);
-        startActivity(numberIntent);
-    }
+        // Find the View that shows the numbers,family,colors and phrases category
+        TextView numbersTextView = (TextView) findViewById(R.id.numbers);
+        TextView familyTextView = (TextView) findViewById(R.id.family);
+        TextView colorsTextView = (TextView) findViewById(R.id.colors);
+        TextView phrasesTextView = (TextView) findViewById(R.id.phrases);
 
-    public void openFamilyList(View view) {
-        Intent familyIntent = new Intent(this, FamilyActivity.class);
-        startActivity(familyIntent);
-    }
+        // Set a click listener on Numbers View
+        numbersTextView.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent numberIntent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(numberIntent);
+            }
+        });
 
-    public void openColorList(View view) {
-        Intent colorIntent = new Intent(this, ColorsActivity.class);
-        startActivity(colorIntent);
-    }
-
-    public void openPhrasesList(View view) {
-        Intent phrasesIntent = new Intent(this, PhrasesActivity.class);
-        startActivity(phrasesIntent);
     }
 }
