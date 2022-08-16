c2cef535595cc62e9873f3453dcdfbc2ec570f73
Junaid Iqbal <Junaid.jigar761@gmail.com>
2022-08-16 19:58:51 +0500

    English words added in Numbers activity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,7 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,5 +11,18 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //String array of English words
+        String [] words = new String[10];
+        words[0] = "One";
+        words[1] = "Two";
+        words[2] = "Three";
+        words[3] = "Four";
+        words[4] = "Five";
+        words[5] = "Six";
+        words[6] = "Seven";
+        words[7] = "Eight";
+        words[8] = "Nine";
+        words[9] = "Ten";
     }
 }
