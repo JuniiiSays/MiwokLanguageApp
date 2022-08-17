ced277af0c80a460a7b9a4070f6093e957ed2c93
Junaid Iqbal <Junaid.jigar761@gmail.com>
2022-08-17 10:29:32 +0500

    Array is replaced by ArrayList in Numbers activity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
@@ -13,7 +15,7 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
         //Array of String for English words
-        String [] words = new String[10];
+        /*String [] words = new String[10];
         words[0] = "One";
         words[1] = "Two";
         words[2] = "Three";
@@ -23,6 +25,19 @@ public class NumbersActivity extends AppCompatActivity {
         words[6] = "Seven";
         words[7] = "Eight";
         words[8] = "Nine";
-        words[9] = "Ten";
+        words[9] = "Ten";*/
+        //ArrayList of String for english words
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("One");
+        words.add("Two");
+        words.add("Three");
+        words.add("Four");
+        words.add("Five");
+        words.add("Six");
+        words.add("Seven");
+        words.add("Eight");
+        words.add("Nine");
+        words.add("Ten");
+
     }
 }
