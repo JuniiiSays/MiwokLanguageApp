c2ae606ed56acc310585040730930d7f347e16e5
Junaid Iqbal <Junaid.jigar761@gmail.com>
2022-08-17 10:54:29 +0500

    Retrieving dada from ArrayList

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -14,18 +16,6 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        //Array of String for English words
-        /*String [] words = new String[10];
-        words[0] = "One";
-        words[1] = "Two";
-        words[2] = "Three";
-        words[3] = "Four";
-        words[4] = "Five";
-        words[5] = "Six";
-        words[6] = "Seven";
-        words[7] = "Eight";
-        words[8] = "Nine";
-        words[9] = "Ten";*/
         //ArrayList of String for english words
         ArrayList<String> words = new ArrayList<String>();
         words.add("One");
@@ -39,5 +29,49 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("Nine");
         words.add("Ten");
 
+        //Find the rootView of Numbers activity
+        LinearLayout rootView = findViewById(R.id.rootView);
+        //Create a TextView programmatically
+        TextView wordView = new TextView(this);
+        //Show first element of the ArrayList ib that TextView
+        wordView.setText(words.get(0));
+        //Attach TextView with the rootView
+        rootView.addView(wordView);
+
+        TextView wordView2 = new TextView(this);
+        wordView2.setText(words.get(1));
+        rootView.addView(wordView2);
+
+        TextView wordView3 = new TextView(this);
+        wordView3.setText(words.get(2));
+        rootView.addView(wordView3);
+
+        TextView wordView4 = new TextView(this);
+        wordView4.setText(words.get(3));
+        rootView.addView(wordView4);
+
+        TextView wordView5 = new TextView(this);
+        wordView5.setText(words.get(4));
+        rootView.addView(wordView5);
+
+        TextView wordView6 = new TextView(this);
+        wordView6.setText(words.get(5));
+        rootView.addView(wordView6);
+
+        TextView wordView7 = new TextView(this);
+        wordView7.setText(words.get(6));
+        rootView.addView(wordView7);
+
+        TextView wordView8 = new TextView(this);
+        wordView8.setText(words.get(7));
+        rootView.addView(wordView8);
+
+        TextView wordView9 = new TextView(this);
+        wordView9.setText(words.get(8));
+        rootView.addView(wordView9);
+
+        TextView wordView10 = new TextView(this);
+        wordView10.setText(words.get(9));
+        rootView.addView(wordView10);
     }
 }
